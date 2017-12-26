5afad19b0825b02d641d5af641791857ab9855b2
nvb <netsenko@gmail.com>
2017-12-27 00:17:44 +0200

    .

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -15,11 +15,21 @@ public class Main : MonoBehaviour {
     [SerializeField]
     private MeshFilter cubeX;
     [SerializeField]
+    private MeshFilter cubeV0;
+    [SerializeField]
     private MeshFilter cubeV1;
     [SerializeField]
     private MeshFilter cubeV2;
     [SerializeField]
     private MeshFilter cubeRes;
+    [SerializeField]
+    private MeshFilter cubeRes2;
+    [SerializeField]
+    private MeshFilter cubeRes3;
+    [SerializeField]
+    private MeshFilter cubeRes4;
+    [SerializeField]
+    private MeshFilter cubeRes5;
     [SerializeField]
     private MeshCollider plane;
     // Use this for initialization
@@ -27,18 +37,33 @@ public class Main : MonoBehaviour {
     private float D = 1.5f;
     private Vector3 Np = new Vector3(0, 1, 0);
 
+    private Vector3[] _vertices;
+    private int[] _triangles;
+
+    private int[] _trilinks;
+
     void Start () {
         Debug.Log("");
         cube.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
         cubeP.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
         cubeX.GetComponent<Renderer>().material.color = new Color(0, 0.5f, 0.5f);
         cubeRes.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
-        cubeV1.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
-        cubeV2.GetComponent<Renderer>().material.color = new Color(0.3f, 0.3f, 0.3f);
-//        Vector3 v = default(Vector3);
-        Debug.Log(DateTime.Now.ToString()/* ToLongTimeString()*/);
-        int[] trisLinks = createTriangleLinks(cap1.mesh);
+        cubeRes2.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+        cubeRes3.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+        cubeRes4.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+        cubeRes5.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+        cubeV0.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+        cubeV1.GetComponent<Renderer>().material.color = new Color(0.2f, 0.2f, 0.2f);
+        cubeV2.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);
+        //        Vector3 v = default(Vector3);
+
+        Debug.Log(DateTime.Now.ToString());
+        _trilinks = createTriangleLinks(cap1.mesh);
         Debug.Log(DateTime.Now.ToLongTimeString());
+
+        _vertices = cap1.mesh.vertices;
+        _triangles = cap1.mesh.triangles;
+
         Debug.DrawRay(cap1.mesh.vertices[cap1.mesh.triangles[0]], cap1.mesh.normals[cap1.mesh.triangles[0]], new Color(1, 0, 0), 1000);
         Debug.DrawRay(cap1.mesh.vertices[cap1.mesh.triangles[1]], cap1.mesh.normals[cap1.mesh.triangles[1]], new Color(1, 0, 0), 1000);
         Debug.DrawRay(cap1.mesh.vertices[cap1.mesh.triangles[2]], cap1.mesh.normals[cap1.mesh.triangles[2]], new Color(1, 0, 0),1000);
@@ -91,7 +116,7 @@ public class Main : MonoBehaviour {
                 res = i;
                 break;
             }*/
-            if ((verts[tris[i3]] == verts[v1] || verts[tris[i3 + 1]] == verts[v1] || verts[tris[i3 + 2]] == verts[v1]) && (verts[tris[i3]] == verts[v2] || verts[tris[i3 + 1]] == verts[v2] || verts[tris[i3 + 2]] == verts[v2]) && i3 != excludeTrisIndex)
+            if ((verts[tris[i3]] == verts[v1] || verts[tris[i3 + 1]] == verts[v1] || verts[tris[i3 + 2]] == verts[v1]) && (verts[tris[i3]] == verts[v2] || verts[tris[i3 + 1]] == verts[v2] || verts[tris[i3 + 2]] == verts[v2]) && i != excludeTrisIndex)
             {
                 res = i;
                 break;
@@ -146,22 +171,94 @@ public class Main : MonoBehaviour {
         cubeP.transform.SetPositionAndRotation(P, cubeP.transform.rotation);
         cubeX.transform.SetPositionAndRotation(X, cubeX.transform.rotation);
 
-        Vector3 I1 = edgeIntersect(P, A, X, v1, v0);
-        Vector3 I2 = edgeIntersect(P, A, X, v1, v2);
-        Vector3 I3 = edgeIntersect(P, A, X, v2, v0);
+        int edgeIndex = 0;
+        Vector3 I0 = edgeIntersect(P, A, X, v1, v0);
+        Vector3 I1 = edgeIntersect(P, A, X, v1, v2);
+        Vector3 I2 = edgeIntersect(P, A, X, v2, v0);
 
-        Vector3 I = I1;
+        Vector3 I = I0;
+        if (I1.y < I.y) {
+            I = I1;
+            edgeIndex = 1;
+        }
         if (I2.y < I.y) {
             I = I2;
-        }
-        if (I3.y < I.y) {
-            I = I3;
+            edgeIndex = 2;
         }
         if (I.y != float.PositiveInfinity) {
             cubeRes.transform.SetPositionAndRotation(I, cubeRes.transform.rotation);
         }
+
+        // cubeRes.transform.SetPositionAndRotation(I1, cubeRes.transform.rotation);
+        int qq;
+        Vector3 QQ;
+        int tt = _trilinks[hit.triangleIndex * 3 + edgeIndex];
+        processTriangle(tt, I, out qq, out QQ);
+        if (QQ != Vector3.positiveInfinity) {
+            cubeRes2.transform.SetPositionAndRotation(QQ, cubeRes2.transform.rotation);
+        } else {
+            Debug.LogError("ups");
+        }
         
-       // cubeRes.transform.SetPositionAndRotation(I1, cubeRes.transform.rotation);
+        int qqq;
+        Vector3 QQQ;
+        int ttt = _trilinks[tt * 3 + qq];
+        processTriangle(ttt, QQ, out qqq, out QQQ);
+        if (QQQ != Vector3.positiveInfinity)
+        {
+            cubeRes3.transform.SetPositionAndRotation(QQQ, cubeRes3.transform.rotation);
+        }
+        int qqqq;
+        Vector3 QQQQ;
+        int tttt = _trilinks[ttt * 3 + qqq];
+        processTriangle(tttt, QQQ, out qqqq, out QQQQ);
+        cubeRes4.transform.SetPositionAndRotation(QQQQ, cubeRes4.transform.rotation);
+        int qqqqq;
+        Vector3 QQQQQ;
+        int ttttt = _trilinks[tttt * 3 + qqqq];
+        processTriangle(ttttt, QQQQ, out qqqqq, out QQQQQ);
+        cubeRes5.transform.SetPositionAndRotation(QQQQQ, cubeRes5.transform.rotation);
+    }
+
+    private void processTriangle(int triN, Vector3 A, out int edgeN, out Vector3 Anew) {
+        Vector3 V01 = _vertices[_triangles[triN * 3 + 1]] - _vertices[_triangles[triN * 3 + 0]];
+        Vector3 V02 = _vertices[_triangles[triN * 3 + 2]] - _vertices[_triangles[triN * 3 + 0]];
+        Vector3 v0 = _vertices[_triangles[triN * 3 + 0]];
+        Vector3 v1 = _vertices[_triangles[triN * 3 + 1]];
+        Vector3 v2 = _vertices[_triangles[triN * 3 + 2]];
+
+        cubeV0.transform.SetPositionAndRotation(v0, cubeV0.transform.rotation);
+        cubeV1.transform.SetPositionAndRotation(v1, cubeV1.transform.rotation);
+        cubeV2.transform.SetPositionAndRotation(v2, cubeV2.transform.rotation);
+
+        Vector3 n = Vector3.Cross(V01, V02);
+
+        int k = 1;
+
+        Vector3 N = n.normalized;
+        Vector3 X = A + k * N;
+        Vector3 P = X - (Vector3.Dot(Np, X) + D) * Np;
+        Debug.DrawLine(X, P, new Color(0, 0, 1));
+
+        int edgeIndex = 0;
+        Vector3 I0 = edgeIntersect(P, A, X, v1, v0);
+        Vector3 I1 = edgeIntersect(P, A, X, v1, v2);
+        Vector3 I2 = edgeIntersect(P, A, X, v2, v0);
+
+        Vector3 I = I0;
+        if (I1.y < I.y) {
+            I = I1;
+            edgeIndex = 1;
+        }
+        if (I2.y < I.y) {
+            I = I2;
+            edgeIndex = 2;
+        }
+      /*  if (I == A) {
+            Debug.Log("upsss");
+        }*/
+        edgeN = edgeIndex;
+        Anew = I;
     }
 
     private Vector3 edgeIntersect(Vector3 P, Vector3 A, Vector3 X, Vector3 T1, Vector3 T2) {
