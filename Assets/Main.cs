a4181a38cd4e15db1eeb4c5e347b21847b1a5303
nvb <netsenko@gmail.com>
2018-01-02 15:24:01 +0200

    add doubleSpheres

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -6,28 +6,12 @@ using UnityEngine;
 public class Main : MonoBehaviour {
     [SerializeField]
     private MeshFilter cap1;
+
     [SerializeField]
-    private MeshFilter cap2;
+    private MeshFilter doubleSphere_tri;
     [SerializeField]
-    private MeshFilter cubeP;
-    [SerializeField]
-    private MeshFilter cubeX;
-    [SerializeField]
-    private MeshFilter cubeV0;
-    [SerializeField]
-    private MeshFilter cubeV1;
-    [SerializeField]
-    private MeshFilter cubeV2;
-    [SerializeField]
-    private MeshFilter cubeRes;
-    [SerializeField]
-    private MeshFilter cubeRes2;
-    [SerializeField]
-    private MeshFilter cubeRes3;
-    [SerializeField]
-    private MeshFilter cubeRes4;
-    [SerializeField]
-    private MeshFilter cubeRes5;
+    private MeshFilter doubleSphere;
+
     [SerializeField]
     private MeshCollider plane;
     // Use this for initialization
@@ -39,39 +23,55 @@ public class Main : MonoBehaviour {
     private int[] _triangles;
 
     private int[] _trilinks;
+    private int[] _trilinks_doubleSphere;
+    private int[] _trilinks_doubleSphere_tri;
 
 
     private GeomProcessor _testGeomProcessor;
+    private GeomProcessor _testGeomProcessor_doubleSphere;
+    private GeomProcessor _testGeomProcessor_doubleSphere_tri;
     [SerializeField]
     private Transform cube;
     private List<Transform> cubes = new List<Transform>();
 
     void Start () {
         Application.targetFrameRate = 60;
- /*       cube.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
-        cubeP.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
-        cubeX.GetComponent<Renderer>().material.color = new Color(0, 0.5f, 0.5f);
-        cubeRes.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
-        cubeRes2.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
-        cubeRes3.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
-        cubeRes4.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
-        cubeRes5.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
-        cubeV0.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
-        cubeV1.GetComponent<Renderer>().material.color = new Color(0.2f, 0.2f, 0.2f);
-        cubeV2.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);*/
+        /*       cube.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+               cubeP.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+               cubeX.GetComponent<Renderer>().material.color = new Color(0, 0.5f, 0.5f);
+               cubeRes.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+               cubeRes2.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+               cubeRes3.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+               cubeRes4.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+               cubeRes5.GetComponent<Renderer>().material.color = new Color(0.75f, 0, 0);
+               cubeV0.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+               cubeV1.GetComponent<Renderer>().material.color = new Color(0.2f, 0.2f, 0.2f);
+               cubeV2.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f);*/
         //        Vector3 v = default(Vector3);
 
+/*        _trilinks = GeomPreprocessor.CreateTrilinks(doubleSphere.mesh);
+        String str = String.Join(",", _trilinks);
+
+        int[] tri = GeomPreprocessor.CreateTrilinks(doubleSphere_tri.mesh);
+        String str_tri = String.Join(",", tri);
+        */
         Debug.Log(DateTime.Now.ToString());
         //  _trilinks = GeomPreprocessor.CreateTrilinks(cap1.mesh);
         //  String str = String.Join(",", _trilinks);
         TextAsset txt = Resources.Load("Trilinks/capsule") as TextAsset;
         _trilinks = Array.ConvertAll(txt.text.Split(','), int.Parse);
+        txt = Resources.Load("Trilinks/doubleSphere") as TextAsset;
+        _trilinks_doubleSphere = Array.ConvertAll(txt.text.Split(','), int.Parse);
+        txt = Resources.Load("Trilinks/doubleSphere_tri") as TextAsset;
+        _trilinks_doubleSphere_tri = Array.ConvertAll(txt.text.Split(','), int.Parse);
         Debug.Log(DateTime.Now.ToLongTimeString()+","+_trilinks.ToString());
 
         _vertices = cap1.mesh.vertices;
         _triangles = cap1.mesh.triangles;
 
         _testGeomProcessor = new GeomProcessor(cap1.mesh, _trilinks);
+        _testGeomProcessor_doubleSphere = new GeomProcessor(doubleSphere.mesh, _trilinks_doubleSphere);
+        _testGeomProcessor_doubleSphere_tri = new GeomProcessor(doubleSphere_tri.mesh, _trilinks_doubleSphere_tri);
         Debug.Log("???");
     }
 
@@ -110,7 +110,14 @@ public class Main : MonoBehaviour {
         if (meshCollider == null || meshCollider.sharedMesh == null)
             return;
 
-        List<Vector3> points = _testGeomProcessor.GetEdgeIntersectPoints(hit.point, hit.triangleIndex);
+        List<Vector3> points;
+        if (meshCollider == cap1.GetComponent<MeshCollider>()) {
+            points = _testGeomProcessor.GetEdgeIntersectPoints(hit.point, hit.triangleIndex);
+        } else if (meshCollider == doubleSphere.GetComponent<MeshCollider>()) {
+            points = _testGeomProcessor_doubleSphere.GetEdgeIntersectPoints(doubleSphere.transform.InverseTransformPoint(hit.point), hit.triangleIndex);
+        } else {
+            points = _testGeomProcessor_doubleSphere_tri.GetEdgeIntersectPoints(doubleSphere_tri.transform.InverseTransformPoint(hit.point), hit.triangleIndex);
+        }
 
         Transform cb;
         while (points.Count > cubes.Count) {
@@ -120,7 +127,14 @@ public class Main : MonoBehaviour {
         }
         for (i=0; i<points.Count; i++) {
             cubes[i].GetComponent<Renderer>().enabled = true;
-            cubes[i].transform.SetPositionAndRotation(points[i], Quaternion.identity);
+            if (meshCollider == cap1.GetComponent<MeshCollider>()) {
+                cubes[i].SetParent(cap1.transform);
+            } else if (meshCollider == doubleSphere.GetComponent<MeshCollider>()) {
+                cubes[i].SetParent(doubleSphere.transform);
+            } else {
+                cubes[i].SetParent(doubleSphere_tri.transform);
+            }
+            cubes[i].transform.localPosition = points[i];
         }
     }
 
