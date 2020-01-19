314f02eb77fbda7a578f5bfe5d1e096f96485ab1
nvb <netsenko@gmail.com>
2020-01-19 22:43:24 +0200

    Tanya test

---
+++ Assets/Test/soTest/TestSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Data", menuName ="SO/testSO")]
+public class TestSO : ScriptableObject
+{
+    public int n;
+}
